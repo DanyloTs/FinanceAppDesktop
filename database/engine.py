2c3c34155cc34867a5ad88844c9b0a56c38dc3f8
AstroMate <tsdan51@gmail.com>
2025-04-22 18:32:12 +0300

    completed engine for db ORM

--- database/engine.py
+++ database/engine.py
@@ -1,10 +1,7 @@
 from sqlalchemy import create_engine, text
 from env_var import passw, user, db
-from sqlalchemy.orm import Session, declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base
 
 engine = create_engine(f"mysql+pymysql://{user}:{passw}@localhost/{db}", echo=True)
-
-with engine.connect() as conn:
-    result = conn.execute(text("SELECT idexpenses, expenses_category, expenses_money FROM expenses"))
-    for row in result:
-        print(f"id: {row.idexpenses} category: {row.expenses_category}, money spent: {row.expenses_money}")
+SessionLocal =  sessionmaker(bind=engine)
+Base = declarative_base()
